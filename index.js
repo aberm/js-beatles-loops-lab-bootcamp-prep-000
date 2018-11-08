5da8c9089918cf4910badf6fdb242bcf6783059a
Menachem Aber <abermendel@gmail.com>
2018-11-08 20:12:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 function theBeatlesPlay(m, ins) {
   var z = [];
   for (var i = 0; i < m.length; i++) {
-    z.push(`$(m[i]) plays $(ins[i])`)
+    z.push(`${m[i]} plays ${ins[i]}`)
   }
   return z
 }
